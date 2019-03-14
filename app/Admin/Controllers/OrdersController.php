b06c1721461504f81b35aa2253f23f1c34cf8ed3
jianpan-t <825664649@qq.com>
2019-03-15 02:08:04 +0800

    z众筹结束逻辑

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -11,6 +11,7 @@ use Encore\Admin\Controllers\HasResourceActions;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use Illuminate\Http\Request;
+use App\Services\OrderService;
 
 class OrdersController extends Controller
 {
@@ -101,7 +102,7 @@ class OrdersController extends Controller
         return redirect()->back();
     }
 
-    public function handleRefund(Order $order, HandleRefundRequest $request)
+    public function handleRefund(Order $order, HandleRefundRequest $request,  OrderService $orderService)
     {
         // 判断订单状态是否正确
         if ($order->refund_status !== Order::REFUND_STATUS_APPLIED) {
@@ -115,8 +116,8 @@ class OrdersController extends Controller
             $order->update([
                 'extra' => $extra,
             ]);
-            // 调用退款逻辑
-            $this->_refundOrder($order);
+            // 改为调用封装的方法
+            $orderService->refundOrder($order);
         } else {
             // 将拒绝退款理由放到订单的 extra 字段中
             $extra = $order->extra ?: [];
@@ -131,7 +132,7 @@ class OrdersController extends Controller
         return $order;
     }
 
-    protected function _refundOrder(Order $order)
+    /*protected function _refundOrder(Order $order)
     {
         // 判断该订单的支付方式
         switch ($order->payment_method) {
@@ -186,5 +187,5 @@ class OrdersController extends Controller
                 throw new InternalException('未知订单支付方式：'.$order->payment_method);
                 break;
         }
-    }
+    }*/
 }
