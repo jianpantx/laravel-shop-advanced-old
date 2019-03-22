b645073648096a25971d96e58ea37228df68260a
jianpan-t <825664649@qq.com>
2019-03-23 00:58:47 +0800

    同步商品数据至 Elasticsearch

--- app/Admin/Controllers/CommonProductsController.php
+++ app/Admin/Controllers/CommonProductsController.php
@@ -9,6 +9,7 @@ use Encore\Admin\Layout\Content;
 use App\Models\Category;
 use Encore\Admin\Grid;
 use Encore\Admin\Form;
+use App\Jobs\SyncOneProductToES;
 
 abstract class CommonProductsController extends Controller
 {
@@ -99,6 +100,8 @@ abstract class CommonProductsController extends Controller
 
         $form->saving(function (Form $form) {
             $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
+            $product = $form->model();
+            $this->dispatch(new SyncOneProductToES($product));
         });
 
         return $form;
