dc1242cb655960aa087e510f6c1384d8226432b9
jianpan-t <825664649@qq.com>
2019-03-23 22:54:01 +0800

    封装 Elasticsearch 查询代码

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,6 +9,7 @@ use App\Models\Product;
 use App\Services\CategoryService;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\LengthAwarePaginator;
+use App\SearchBuilders\ProductSearchBuilder;
 
 class ProductsController extends Controller
 {
@@ -75,7 +76,7 @@ class ProductsController extends Controller
 
     public function index(Request $request)
     {
-        $page    = $request->input('page', 1);
+        /*$page    = $request->input('page', 1);
         $perPage = 16;
 
         // 构建查询
@@ -196,7 +197,51 @@ class ProductsController extends Controller
             }
         }
 
-        $result = app('es')->search($params);
+        $result = app('es')->search($params);*/
+
+        $page    = $request->input('page', 1);
+        $perPage = 16;
+        // 新建查询构造器对象，设置只搜索上架商品，设置分页
+        $builder = (new ProductSearchBuilder())->onSale()->paginate($perPage, $page);
+
+        if ($request->input('category_id') && $category = Category::find($request->input('category_id'))) {
+            // 调用查询构造器的类目筛选
+            $builder->category($category);
+        }
+
+        if ($search = $request->input('search', '')) {
+            $keywords = array_filter(explode(' ', $search));
+            // 调用查询构造器的关键词筛选
+            $builder->keywords($keywords);
+        }
+
+        if ($search || isset($category)) {
+            // 调用查询构造器的分面搜索
+            $builder->aggregateProperties();
+        }
+
+        $propertyFilters = [];
+        if ($filterString = $request->input('filters')) {
+            $filterArray = explode('|', $filterString);
+            foreach ($filterArray as $filter) {
+                list($name, $value) = explode(':', $filter);
+                $propertyFilters[$name] = $value;
+                // 调用查询构造器的属性筛选
+                $builder->propertyFilter($name, $value);
+            }
+        }
+
+        if ($order = $request->input('order', '')) {
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $m)) {
+                if (in_array($m[1], ['price', 'sold_count', 'rating'])) {
+                    // 调用查询构造器的排序
+                    $builder->orderBy($m[1], $m[2]);
+                }
+            }
+        }
+
+        // 最后通过 getParams() 方法取回构造好的查询参数
+        $result = app('es')->search($builder->getParams());
 
         // 通过 collect 函数将返回结果转为集合，并通过集合的 pluck 方法取到返回的商品 ID 数组
         $productIds = collect($result['hits']['hits'])->pluck('_id')->all();
